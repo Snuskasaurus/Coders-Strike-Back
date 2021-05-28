e19d42424737cd8d1e444c9e650d1ec8370d72bc
Julien Rogel <66749665+Snuskasaurus@users.noreply.github.com>
2021-05-28 12:23:19 +0200

    Bronze Update
    - Fixed a bug: The constructor of the vector used int instead of float, resulting in the normalized vector being null.
    - Check if the opponent is in front of the player before boosting, need to check the distance later.
    
    RANK : Bronze 4,293 30,505

--- Bronze.cpp
+++ Bronze.cpp
@@ -6,15 +6,20 @@
 
 using namespace std;
 
+#define PI 3.141592f
+
 #define MAP_RADIUS_CHECKPOINT 600.0f
 #define MAP_WIDTH 16000.0f
 #define MAP_HEIGHT 9000.0f
 
-#define BOOST_THRESHOLD_DISTANCE 8500.0f
+#define BOOST_THRESHOLD_DISTANCE 1500.0f
+#define BOOST_THRESHOLD_DOT_OPPONENT 0.90f
 #define BOOST_THRESHOLD_ANGLE 1.0f
 #define BOOST_KEYWORD "BOOST"
 
 #define FLOAT_COMPARE(_a, _b) (fabs(_a - _b) < 0.000001f)
+#define DEG_TO_RAD(angleDeg) (angleDeg * PI / 180.0f)
+#define RAD_TO_DEG(angleRad) (angleRad * 180.0f / PI)
 
 #pragma region Vector2
 
@@ -26,20 +31,19 @@ public:
 	static const Vector2 Right;
 
 	Vector2() = default;
-	Vector2(int _x, int _y = 0) : m_x(_x), m_y(_y) {};
+	Vector2(float _x, float _y = 0.0f) : m_x(_x), m_y(_y) {};
 	Vector2(const Vector2& _other) : m_x(_other.m_x), m_y(_other.m_y) {};
 
 	float m_x = 0;
 	float m_y = 0.;
 
 	static float Dot(const Vector2& _v1, const Vector2& _v2);
-	static float Angle(const Vector2& _from, const Vector2& _to);
 	static float Distance(const Vector2& _a, const Vector2& _b);
 	static float SquareDistance(const Vector2& _a, const Vector2& _b);
-	static Vector2 Normalize(const Vector2& _v);
 
 	float Magnitude() const;
 	inline float SquareMagnitude() const;
+	Vector2 Normalized() const;
 
 	inline bool operator== (const Vector2& _v) const { return (FLOAT_COMPARE(m_x, _v.m_x) && FLOAT_COMPARE(m_y, _v.m_y)); };
 	inline bool operator!= (const Vector2& _v) const { return (!FLOAT_COMPARE(m_x, _v.m_x) || !FLOAT_COMPARE(m_y, _v.m_y)); };
@@ -64,12 +68,6 @@ float Vector2::Dot(const Vector2& _v1, const Vector2& _v2)
 	return (_v1.m_x * _v2.m_x) + (_v1.m_y * _v2.m_y);
 }
 
-float Vector2::Angle(const Vector2& _from, const Vector2& _to)
-{
-	Vector2 vec = _to - _from;
-	return atan2f(vec.m_x, vec.m_y);
-}
-
 float Vector2::Distance(const Vector2& _a, const Vector2& _b)
 {
 	return sqrtf(SquareDistance(_a, _b));
@@ -83,16 +81,6 @@ float Vector2::SquareDistance(const Vector2& _a, const Vector2& _b)
 	return (distanceX * distanceX) + (distanceY * distanceY);
 }
 
-Vector2 Vector2::Normalize(const Vector2& _v)
-{
-	if (_v == Zero) return 0.0f;
-	if (_v.m_x == 0.0f) return Vector2(0.0f, 1.0f);
-	if (_v.m_y == 0.0f) return Vector2(1.0f, 0.0f);
-
-	float magnitude = _v.Magnitude();
-	return Vector2(_v.m_x / magnitude, _v.m_y / magnitude);
-}
-
 float Vector2::Magnitude() const
 {
 	if (m_x == 0.0f && m_y == 0.0f) return 0.0f;
@@ -104,6 +92,17 @@ float Vector2::Magnitude() const
 float Vector2::SquareMagnitude() const
 {
 	return (m_x * m_x) + (m_y * m_y);
+}
+
+Vector2 Vector2::Normalized() const
+{
+	if (*this == Zero) return Zero;
+	if (FLOAT_COMPARE(m_x, 0.0f)) return Vector2(0.0f, 1.0f);
+	if (FLOAT_COMPARE(m_y, 0.0f)) return Vector2(1.0f, 0.0f);
+
+	float magnitude = this->Magnitude();
+	Vector2 vecToReturn = Vector2((m_x / magnitude), (m_y / magnitude));
+	return vecToReturn;
 }
 
 Vector2& Vector2::operator+=(const Vector2& _v)
@@ -178,8 +177,10 @@ public:
 class Pod
 {
 public:
-	virtual void ManageInput();
-	virtual void ManageDebug() {};
+	virtual void UpdateInput();
+	virtual void UpdateDebug() {};
+
+	inline Vector2 GetPosition() const { return m_position; }
 
 protected:
 	Vector2 m_position = Vector2::Zero;
@@ -187,7 +188,7 @@ protected:
 	bool m_canUseBoost = true;
 };
 
-void Pod::ManageInput()
+void Pod::UpdateInput()
 {
 	cin >> m_position.m_x >> m_position.m_y;
 	cin.ignore();
@@ -200,34 +201,46 @@ void Pod::ManageInput()
 class PlayerPod : public Pod
 {
 public:
-	void ManageInput() override;
-	void ManageDebug() override;
-	void ManageOutput();
+	void UpdateInput() override;
+	void UpdateDebug() override;
+	void UpdateValues(const Pod& _opponent);
+	void UpdateOutput();
 
 private:
 	int ComputeThrust();
 	bool CheckShouldBoost();
 
 	Checkpoint m_nextCheckpoint;
+
+	float dotOpponentPlayer = 0.0f;
 };
 
-void PlayerPod::ManageInput()
+void PlayerPod::UpdateInput()
 {
-	cin >> m_position.m_x >> m_position.m_y >> 
-		m_nextCheckpoint.m_position.m_x >> m_nextCheckpoint.m_position.m_y >> 
+	cin >> m_position.m_x >> m_position.m_y >>
+		m_nextCheckpoint.m_position.m_x >> m_nextCheckpoint.m_position.m_y >>
 		m_nextCheckpoint.m_distance >> m_nextCheckpoint.m_angle;
 	cin.ignore();
 }
 
-void PlayerPod::ManageDebug()
+void PlayerPod::UpdateValues(const Pod& _opponent)
+{
+	// Calculate the dot product of the opponent and the player
+	Vector2 playerToCheckpoint = (m_nextCheckpoint.m_position - m_position).Normalized();
+	Vector2 playerToOpponent = (_opponent.GetPosition() - m_position).Normalized();
+	dotOpponentPlayer = Vector2::Dot(playerToCheckpoint, playerToOpponent);
+}
+
+void PlayerPod::UpdateDebug()
 {
 	cerr << "Thrust : " << m_thrust << endl;
 	cerr << "Can use boost : " << m_canUseBoost << endl;
 	cerr << "Distance to checkpoint : " << m_nextCheckpoint.m_distance << endl;
 	cerr << "Angle to checkpoint : " << m_nextCheckpoint.m_angle << endl;
+	cerr << "Dot between opponent and player : " << dotOpponentPlayer << endl;
 }
 
-void PlayerPod::ManageOutput()
+void PlayerPod::UpdateOutput()
 {
 	if (CheckShouldBoost() == true)
 	{
@@ -265,6 +278,9 @@ bool PlayerPod::CheckShouldBoost()
 	bool hasGoodDistance = m_nextCheckpoint.m_distance > BOOST_THRESHOLD_DISTANCE;
 	if (false == hasGoodDistance) return false;
 
+	bool isInFrontOfOpponent = (dotOpponentPlayer > BOOST_THRESHOLD_DOT_OPPONENT);
+	if (isInFrontOfOpponent) return false;
+
 	m_canUseBoost = false;
 	return true;
 }
@@ -280,9 +296,10 @@ int main()
 	bool isRunning = true;
 	while (isRunning)
 	{
-		playerPod.ManageInput();
-		opponent.ManageInput();
-		playerPod.ManageDebug();
-		playerPod.ManageOutput();
+		playerPod.UpdateInput();
+		opponent.UpdateInput();
+		playerPod.UpdateValues(opponent);
+		playerPod.UpdateDebug();
+		playerPod.UpdateOutput();
 	}
 }
